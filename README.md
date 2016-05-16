4f3159749589b48098b5966e99521d3efc5499b1
saulibanez <saul.ibanez.cerro@gmail.com>
2016-05-16 17:49:15 +0200

    hoteles v1

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # X-Nav-Practica-Hoteles
 Repositorio para la práctica final de DAT/AT. Curso 2015-2016
+
+#Para visualilzar la práctica:
+
+http://saulibanez.github.io/X-Nav-Practica-Hoteles
---
+++ js/hoteles.js
@@ -0,0 +1,41 @@
+//https://github.com/CursosWeb/Code/blob/master/JS-APIs/misc/alojamientos/alojamientos.json
+
+function show_accomodation(){
+	var accomodation = accomodations[$(this).attr('no')];
+	var lat = accomodation.geoData.latitude;
+	var lon = accomodation.geoData.longitude;
+	var url = accomodation.basicData.web;
+	var name = accomodation.basicData.name;
+	var desc = accomodation.basicData.body;
+	var img = accomodation.multimedia.media[0].url;
+	var cat = accomodation.extradata.categorias.categoria.item[1]['#text'];
+	var subcat = accomodation.extradata.categorias.categoria.subcategorias.subcategoria.item[1]['#text'];
+	L.marker([lat, lon]).addTo(map).bindPopup('<a href="' + url + '">' + name + '</a><br/>').openPopup();
+	map.setView([lat, lon], 15);
+	$('#desc').html('<h2>' + name + '</h2>'	+ '<p>Type: ' + cat + ', subtype: ' + subcat + '</p>' + desc + '<img src="' + img + '"">');
+};
+
+function get_accomodations(){
+  $.getJSON("alojamientos.json", function(data) {
+    $('#get').html('');
+    accomodations = data.serviceList.service
+    $('#list').after('<h1>' + accomodations.length + '</h1>');
+    var list = '<p>Hotels found: ' + accomodations.length
+     + ' (click on any of them for details and location in the map)</p>'
+    list = list + '<ul>'
+    for (var i = 0; i < accomodations.length; i++) {
+      list = list + '<li no=' + i + '>' + accomodations[i].basicData.title + '</li>';
+    }
+    list = list + '</ul>';
+    $('#list').html(list);
+    $('li').click(show_accomodation);
+  });
+};
+
+$(document).ready(function() {
+	map = L.map('map').setView([40.4175, -3.708], 11);
+	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+	}).addTo(map);
+	$("#get").click(get_accomodations);
+});
